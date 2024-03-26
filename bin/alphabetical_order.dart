76f2645fd13704bd1b21f155831045cfb449a846
Syamkrishna2272 <syamkrishnashyam@gmail.com>
2024-03-26 09:51:28 +0530

    updated

---
+++ bin/alphabetical_order.dart
@@ -0,0 +1,5 @@
+main(){
+  var name=['syam','xavier','varun','nishad'];
+  name.sort((a,b)=>a.toString().compareTo(b.toString()));
+  print(name);
+}
---
+++ bin/descending_order.dart
@@ -0,0 +1,5 @@
+main(){
+  var nums=[4,5,88,9,6,33,10];
+  nums.sort((b,a)=>a.compareTo(b));
+  print(nums);
+}
