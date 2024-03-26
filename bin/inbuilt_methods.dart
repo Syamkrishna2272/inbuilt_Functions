dc727a147862fdcdb941de91865ba71768f794c9
Syamkrishna2272 <syamkrishnashyam@gmail.com>
2024-03-26 09:44:22 +0530

    first commit

---
+++ bin/inbuilt_methods.dart
@@ -0,0 +1,5 @@
+main(){
+  var number=[10,25,3,6,4,88,63];
+  number.sort((a,b)=>a.compareTo(b));
+  print(number);
+}
---
+++ test/inbuilt_methods_test.dart
@@ -0,0 +1,8 @@
+import 'package:inbuilt_methods/inbuilt_methods.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
